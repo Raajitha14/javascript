653713381f937d7b4d394cce1367dca9e40b4e45
Raajitha14 <136095022+Raajitha14@users.noreply.github.com>
2025-07-12 17:27:00 +0000

    datatypes

---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+
+    3)   //code readability should be high
+
+console.log("Rajitha")
+
+let name="Rajitha"   //string
+let age=18      //number
+let isLoggedIn=false  //boolean
+
+let state=null  //null , not undefined ,but empty
+
+//DATATYPES
+//number =>  2^53
+//bigint
+//string=> " "
+//boolean =>  true/false
+//null =>standalone value 
+//undefined => value not defined
+//symbol => unique
+
+//object
+
+
+console.log(typeof "rajitha")
+console.log(typeof age);
+
+console.log(typeof null);    //object
+
+console.log(typeof undefined); //undefimed
+
+
+
+
+
