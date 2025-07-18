24b2e88e89bed63d4ce8ae68872766647921bb71
Raajitha14 <136095022+Raajitha14@users.noreply.github.com>
2025-07-18 03:34:51 +0000

    object1

--- 02_BASICS/03_objects.js
+++ 02_BASICS/03_objects.js
@@ -18,14 +18,37 @@ const JsUser={
     lastLoginDays:["monday","saturday"]
 }
 
-console.log(JsUser.name);  //not good practice to access the values
+// console.log(JsUser.name);  //not good practice to access the values
 
-console.log(JsUser["email"]);  //good practice
-
-
-// console.log(JsUser.full name);  //cannot do,there is a space
-
-console.log(JsUser["full name"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser["email"]);  //good practice
 
 
+// // console.log(JsUser.full name);  //cannot do,there is a space
+
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+// JsUser.email="rajitha@google.com"
+
+// Object.freeze(JsUser)   //If we freeze then any changes done will not be propagated
+
+JsUser.email="rajitha@microsoft.com"   //This will not be propagated
+// console.log(JsUser["email"]);    //prev one--rajitha@google.com
+
+
+// console.log(JsUser);
+
+JsUser.greeting=function(){
+
+    console.log("Hello JS");
+
+}
+console.log(JsUser.greeting);    //reference of function
+
+console.log(JsUser.greeting());
+
+JsUser.greetingTwo=function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+console.log(JsUser.greetingTwo());
