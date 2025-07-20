6b3f1183aa686d615e1a3c6debddfa030e5708be
Raajitha14 <136095022+Raajitha14@users.noreply.github.com>
2025-07-20 17:14:48 +0000

    fun2

--- 03_BASICS/01_functions.js
+++ 03_BASICS/01_functions.js
@@ -79,11 +79,67 @@ const res=addTwoNumbers(3,5)
 
  
  //If we want to avoid such cases of empty argument/ no username we can pass some default values:
-function loginUserMessage(username="Sam"){  //if username is not there, then it will consider sam as username
+// function loginUserMessage(username="Sam"){  //if username is not there, then it will consider sam as username
     
-         return  `${username} just logged in`
- }
+//          return  `${username} just logged in`
+//  }
 
- console.log(loginUserMessage())
- console.log(loginUserMessage("Raj"));
+//  console.log(loginUserMessage())
+//  console.log(loginUserMessage("Raj"));
  
+//-------------------------------------
+
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200,400,500))   //o/p: 200
+// //That's why we should use rest operatot
+
+
+//USING REST OPERATOR
+function calculateCartPrice(...num1){    
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500,2000))   //now we get the array
+
+
+
+function calculateCartPrice1(val1,val2,...num1){    
+    return num1
+}
+// console.log(calculateCartPrice1(200,400,500,2000))   //val1=200, val2=400, rest all in num1
+
+
+
+
+//PASSING OBJECT INSIDE FUNCTION
+
+const user={
+    username:"rajitha",
+    price:199
+}
+
+function handleObject(anyObject){
+    console.log(`Usename is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+handleObject(user)
+
+//can also pass obj directly
+handleObject({
+    username:"sam",
+    price:399
+})
+
+
+
+//PASSING ARRAYS INSIDE FUNCTION
+const myNewArray=[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
+
